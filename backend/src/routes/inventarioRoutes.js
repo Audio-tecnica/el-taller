6ace267e42795d53bf4544d9507fcd06738954d3
Carlos De leon <cardeleon10@gmail.com>
2026-02-05 09:45:05 -0500

    const { authMiddleware } = require('../middleware/authMiddleware');

--- backend/src/routes/inventarioRoutes.js
+++ backend/src/routes/inventarioRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const inventarioController = require("../controllers/inventarioController");
-const { authMiddleware } = require("../middleware/authMiddleware");  // ⭐ Agregar llaves
+const { authMiddleware } = require('../middleware/authMiddleware');  // ⭐ Agregar llaves
 
 router.use(authMiddleware);
 
